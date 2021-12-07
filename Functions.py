94e20c4963a9135bdbf2bfd9fc4932367b13092a
Abasiubong <ubongabasi09@gmail.com>
2021-12-07 12:54:45 -0800

    Using functions

---
+++ Functions.py
@@ -0,0 +1,19 @@
+def patricia(username):
+    print(f"hello,{username}")
+patricia("udeme")
+
+
+def young_money():
+    print("where is your location?")
+young_money()
+
+
+
+def message(name,age):
+    print(f"my name is {name} and i am {age} years old")
+message(name="udeme",age=45)
+
+
+def blessings (name,age,city,occupation):
+    print(f"my name is {name} and i am {age} years old,i am from{city},i am also a {occupation}")
+blessings("abasiubong",23,"uyo","lawyer")
